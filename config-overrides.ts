f1d30a6449c171acec8f3318c6c73641115f161f
Kolenich <nick.zhigalin@gmail.com>
2021-10-14 00:44:36 +0300

    Initial commit.

---
+++ config-overrides.ts
@@ -0,0 +1,19 @@
+// @ts-ignore
+const customizer = require('customize-cra');
+
+module.exports = customizer.override(
+  customizer.addBabelPlugin([
+    'module-resolver',
+    {
+      alias: {
+        '^components/*': './src/pages/*',
+        '^context/*': './src/context/*',
+        '^types/*': './src/types/*',
+        '^routers/*': './src/routers/*',
+        '^lib/*': './src/lib/*',
+        '^pages/*': './src/pages/*',
+        '^img/*': './src/img/*',
+      },
+    },
+  ]),
+);
