51f62040190d270320e695e6fbee5adc5b98a4f3
Kolenich <nick.zhigalin@gmail.com>
2021-10-17 01:01:27 +0300

    Fixed bug for calculating current week on Sunday.

--- src/pages/MainPage/utils.ts
+++ src/pages/MainPage/utils.ts
@@ -16,13 +16,18 @@ const getWeek = (weeks: Weeks, type: 'current' | 'next' | 'previous') => {
       today = weeks.current[0].clone().add(-7, 'days');
       break;
     case 'current':
+      // Fix bug for JS thinking, that Sunday is the first day of week
+      if (today.day() === 0) {
+        today = today.clone().subtract(1, 'week');
+      }
+      break;
     default:
       break;
   }
 
   for (let i = 1; i <= 7; i += 1) {
     const dayOfCurrWeek = today.clone().set('day', i);
-    const dayOfPreviousWeek = today.clone().add(-7, 'days').set('day', i);
+    const dayOfPreviousWeek = today.clone().subtract(7, 'days').set('day', i);
     const dayOfNextWeek = today.clone().add(7, 'days').set('day', i);
 
     current.push(dayOfCurrWeek);
