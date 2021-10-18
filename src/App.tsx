395d06920a9e23fa641d037238739d87779103bf
Kolenich <nick.zhigalin@gmail.com>
2021-10-19 02:46:27 +0300

    Added snackbar provider.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { LocalizationProvider } from '@mui/lab';
 import DateAdapter from '@mui/lab/AdapterMoment';
 import ContextProvider from 'context';
 import 'moment/locale/ru';
+import { SnackbarProvider } from 'notistack';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import RootRouter from 'routers/RootRouter';
@@ -10,11 +11,13 @@ import APIProvider from './api';
 const App = () => (
   <BrowserRouter>
     <LocalizationProvider dateAdapter={DateAdapter}>
-      <APIProvider>
-        <ContextProvider>
-          <RootRouter/>
-        </ContextProvider>
-      </APIProvider>
+      <SnackbarProvider>
+        <APIProvider>
+          <ContextProvider>
+            <RootRouter/>
+          </ContextProvider>
+        </APIProvider>
+      </SnackbarProvider>
     </LocalizationProvider>
   </BrowserRouter>
 );
