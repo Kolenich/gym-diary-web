2c9a743136f4005bf141979b275492598eb29a7c
Kolenich <nick.zhigalin@gmail.com>
2023-09-15 02:10:44 +0300

    Added module federation plugin.

---
+++ src/bootstrap.tsx
@@ -0,0 +1,18 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import 'moment/locale/ru';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const container = document.getElementById('root') as HTMLDivElement;
+const root = createRoot(container);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
