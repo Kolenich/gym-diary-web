c929eaec15ca7ba87ad1e4cf6ecfea5905196a72
Kolenich <nick.zhigalin@gmail.com>
2023-09-15 04:09:53 +0300

    Removed ModuleFederation plugin.

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const CircularDependencyPlugin = require('circular-dependency-plugin');
-const { ModuleFederationPlugin } = require('webpack').container;
-const dependencies = require('./package.json');
 
 module.exports = {
   webpack: {
@@ -22,25 +20,6 @@ module.exports = {
         failOnError: true,
         allowAsyncCycles: false,
         cwd: process.cwd(),
-      }),
-      new ModuleFederationPlugin({
-        name: 'gym_diary',
-        filename: 'remoteEntry.js',
-        library: { type: 'var', name: 'gym_diary' },
-        exposes: {
-          './App': './src/App',
-        },
-        shared: {
-          react: { singleton: true, requiredVersion: dependencies.react },
-          'react-dom': { singleton: true, requiredVersion: dependencies['react-dom'] },
-          'react-router-dom': { singleton: true, requiredVersion: dependencies['react-router-dom'] },
-          '@emotion/react': { singleton: true, requiredVersion: dependencies['@emotion/react'] },
-          '@emotion/styled': { singleton: true, requiredVersion: dependencies['@emotion/styled'] },
-          '@fontsource/roboto': { singleton: true, requiredVersion: dependencies['@fontsource/roboto'] },
-          '@mui/icons-material': { singleton: true, requiredVersion: dependencies['@mui/icons-material'] },
-          '@mui/material': { singleton: true, requiredVersion: dependencies['@mui/material'] },
-          '@mui/x-date-pickers': { singleton: true, requiredVersion: dependencies['@mui/x-date-pickers'] },
-        },
       }),
     ],
   },
