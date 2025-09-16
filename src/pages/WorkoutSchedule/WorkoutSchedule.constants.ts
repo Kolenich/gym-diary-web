d5233149f4e344a5995a2e6a766ea9e41997055e
Kolenich <zhigalin.nick@yandex.ru>
2025-09-17 02:26:23 +0300

    Test commit.

--- src/pages/WorkoutSchedule/WorkoutSchedule.constants.ts
+++ src/pages/WorkoutSchedule/WorkoutSchedule.constants.ts
@@ -1,4 +1,4 @@
-export const SCHEDULE_TITLE = 'План говна';
+export const SCHEDULE_TITLE = 'План тренировок';
 export const WORKOUTS_TITLE = 'Тренировки';
 export const CREATE_WORKOUT = 'Создать тренировку';
 export const CHOOSE_DATE = 'Выберите дату';
